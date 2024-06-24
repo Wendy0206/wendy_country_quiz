7e12471daa3d37676075836a5d24298f335ae433
Wendy Donley Pierre <155206385+Wendy0206@users.noreply.github.com>
2024-06-24 04:13:16 +0000

    95% done

--- vite-capital/src/quizapp.jsx
+++ vite-capital/src/quizapp.jsx
@@ -6,35 +6,56 @@ const QuizApp = () => {
   const goodAnswer = useRef('');
   const score = useRef(0);
   const questionIndex = useRef(0);
-  const quizQuestions=useRef([]);
-
+  const quizQuestions = useRef([]);
+  const [count, setCount] = useState(null);
   const [answers, setAnswers] = useState(['', '', '', '']);
   const [currentUrl, setCurrentUrl] = useState('http://purecatamphetamine.github.io/country-flag-icons/3x2/US.svg');
 
-  const all_Questions = ['BD', 'AL','US','DO', 'DZ', 'AS', 'KH', 'CN', 'EG', 'SV', 'ET', 'CA'];
-  
-  let timeLeft = 20;
-  let timerInterval;
+  const all_Questions = ['BD', 'AL', 'US', 'DO', 'DZ', 'AS', 'KH', 'CN', 'EG', 'SV', 'ET', 'CA'];
 
  
-  const startQuiz = () => {
+  useEffect(() => {
+    let timerInterval;
+    if(count>=0){
+      timerInterval = setInterval(function() {
+       setCount(count-1);
     
+        // End the quiz if time runs out
+        if (count==1) {
+        
+         displayQuestion();
+         setCount(20);
+        }
+      }, 1000);
+
+      return () => {
+        clearInterval(timerInterval);
+     
+      }
+    }
+  }, [count]);
+ 
+
+  const startQuiz = () => {
+
     document.getElementById("start-button").style.display = "none";
     document.getElementById("all_answers").style.display = "block";
+    document.querySelector("span").style.display = "block";
+    document.querySelector("p").style.display = "block";
     let shuffled = all_Questions.sort(() => 0.5 - Math.random());
-    quizQuestions.current=shuffled.slice(0, 10); // here we get 3 other random answers;
+    quizQuestions.current = shuffled.slice(0, 10); // here we get 3 other random answers;
+  setCount(20);
     displayQuestion();
-    startTimer();
+    
   }
 
-
-
   function displayQuestion() {
-  let current_one=quizQuestions.current[questionIndex.current];
-    let flagtest = 'http://purecatamphetamine.github.io/country-flag-icons/3x2/' +current_one + '.svg';
+  
+    let current_one = quizQuestions.current[questionIndex.current];
+    let flagtest = 'http://purecatamphetamine.github.io/country-flag-icons/3x2/' + current_one + '.svg';
 
     setCurrentUrl(flagtest);
-    let clone_array= quizQuestions.current;
+    let clone_array = quizQuestions.current;
 
     const shuffled = clone_array.sort(() => 0.5 - Math.random());
 
@@ -48,104 +69,81 @@ const QuizApp = () => {
   }
 
 
-  // Function to start the timer
-  function startTimer() {
-    timerInterval = setInterval(function () {
-      timeLeft--;
-
-      // Update the timer text
-      document.getElementById("timer").textContent = timeLeft;
-
-      // End the quiz if time runs out
-      if (timeLeft <= 0) {
-        endQuiz();
-      }
-    }, 1000);
-  }
-
-  // Function to end the quiz
-  function endQuiz() {
-    // Stop the timer
-    clearInterval(timerInterval);
-
-  }
-
-
-  const check_answers = (answer,e) => {
+  const check_answers = (answer, e) => {
+    
     if (answers[answer] == goodAnswer.current) {
-      score.current= score.current+1;
-    }
-    else{
-   
+      score.current = score.current + 1;
     }
 
-    questionIndex.current= questionIndex.current+1;
-   
+    questionIndex.current = questionIndex.current + 1;
+
     if (questionIndex.current <= 9) {
+      setCount(20);
       displayQuestion();
     } else {
+      clearInterval(timerInterval);
       const dialog = document.getElementById('modal_dialog');
       dialog.showModal();
     }
   }
 
-  const reset_game=()=>{
+  const reset_game = () => {
     document.getElementById("start-button").style.display = "block";
     document.getElementById("all_answers").style.display = "none";
-    score.current=0;
+    document.querySelector("span").style.display = "none";
+    score.current = 0;
     setCurrentUrl('http://purecatamphetamine.github.io/country-flag-icons/3x2/US.svg');
-    scorePercentage.current=0;
+    scorePercentage.current = 0;
   }
-  
+
 
   return (
+    <div className='container_div'>
 
-    <div className='myApp'>
-      <h2>Lets Play</h2>
-      <div className="quiz-container">
-        <h3>Here we go</h3>
-        <img alt="United States" src={currentUrl} />
-        <div id="question-container">
-          <p id="question-text">Hey! Which country is this?</p>
-          <div id='all_answers'>
-            <div id="answer-buttons">
-              <button className='answer-button' onClick={(e) => check_answers(0,e)}>{answers[0]}</button>
-              <button className='answer-button' onClick={(e) => check_answers(1,e)}>{answers[1]}</button>
-              <button className='answer-button' onClick={(e) => check_answers(2,e)}>{answers[2]}</button>
-              <button className='answer-button' onClick={(e) => check_answers(3,e)}>{answers[3]}</button>
-            </div>
-          </div>
-        </div>
-        <div id="controls-container">
-          <button id="start-button" onClick={() => startQuiz()}>Start Quiz</button>
-          <div id="timer-container">
-            <span id="timer-text">Time Left: <span id="timer">0</span></span>
+    <div className="quiz-container">
+      <h3>History</h3>
+      <img alt="United States" src={currentUrl} />
+      <div id="question-container">
+        <p id="question-text">Hey! Which country is this?</p>
+        <div id='all_answers'>
+          <div id="answer-buttons">
+            <button className='answer-button' onClick={(e) => check_answers(0, e)}>{answers[0]}</button>
+            <button className='answer-button' onClick={(e) => check_answers(1, e)}>{answers[1]}</button>
+            <button className='answer-button' onClick={(e) => check_answers(2, e)}>{answers[2]}</button>
+            <button className='answer-button' onClick={(e) => check_answers(3, e)}>{answers[3]}</button>
           </div>
         </div>
       </div>
+      <div id="controls-container">
+        <button id="start-button" onClick={() => startQuiz()}>Start Quiz</button>
+        <div id="timer-container">
+          <span id="timer-text">Time Left: {count}</span>
+        </div>
+      </div>
+
 
       <dialog id="modal_dialog" className="rounded dialog_margin">
 
-<div className="modal-content">
-  <div className="modal-body">
-  <h2>Quiz Completed!</h2>
-    <p>Your Score: {score.current} out of 10</p>
-     </div>
-  <div className="modal-footer d-flex justify-content-center">
-    <button type="button" className="btn btn-secondary" data-dismiss="modal" style={{ fontFamily: "arial" }} onClick={() => {
-    
-      const dialog = document.getElementById('modal_dialog');
+        <div className="modal-content">
+          <div className="modal-body">
+            <h2>Quiz Completed!</h2>
+            <p>Your Score: {score.current} out of 10</p>
+          </div>
+          <div className="modal-footer d-flex justify-content-center">
+            <button type="button" className="btn btn-secondary" data-dismiss="modal" style={{ fontFamily: "arial" }} onClick={() => {
 
-      dialog.close();
-     reset_game();
-    }}>Close</button>
-  </div>
+              const dialog = document.getElementById('modal_dialog');
 
-</div>
+              dialog.close();
+              reset_game();
+            }}>Close</button>
+          </div>
 
-</dialog>
+        </div>
+
+      </dialog>
     </div>
-
+    </div>
   )
 };
 
